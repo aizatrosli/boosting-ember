f99f707e1ba56286a661e9e507a27c760c66d916
Ahmad Aizat <ahmadaizatr@gmail.com>
2021-05-05 10:09:16 +0800

    catboost bug

--- scripts/catboost_ember.py
+++ scripts/catboost_ember.py
@@ -1,5 +1,7 @@
+import os,sys
+
+sys.path.append(os.path.dirname(os.getcwd()))
 from boostember import *
 
-run = Boosting('testing catboost', booster='cb',
-               dataset='/home/aizat/OneDrive/Master Project/Workspace/dataset/ember2018', n_jobs=22)
+run = Boosting('testing catboost', booster='cb', dataset='/home/aizat/OneDrive/Master Project/Workspace/dataset/ember2018', n_jobs=22)
 run.main(cv=False)
